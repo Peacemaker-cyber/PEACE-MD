4b8f3f5c6eb6b91bdb9a389f01aca374274b4c5e
Kayboy15 <takundaamin074@gmail.com>
2025-06-01 12:41:09 +0200

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "PEACEMAKER~QYZgXT7Z#dQRRIM76C3GKhm3t6VHiRLb3rfbHu5UecX4z2boPMzY",
 // add your Session Id 
 AUTO_STATUS_SEEN: process.env.AUTO_STATUS_SEEN || "true",
 // make true or false status auto seen
@@ -27,7 +27,7 @@ MENU_IMAGE_URL: process.env.MENU_IMAGE_URL || "https://files.catbox.moe/n0dgjr.j
 // add custom menu and mention reply image url
 PREFIX: process.env.PREFIX || ".",
 // add your prifix for bot   
-BOT_NAME: process.env.BOT_NAME || "PEACE-MD",
+BOT_NAME: process.env.BOT_NAME || "KEITH-MD",
 // add bot namw here for menu
 STICKER_NAME: process.env.STICKER_NAME || "PEACE-MD",
 // type sticker pack name 
@@ -37,11 +37,11 @@ CUSTOM_REACT_EMOJIS: process.env.CUSTOM_REACT_EMOJIS || "💝,💖,💗,❤️�
 // chose custom react emojis by yourself 
 DELETE_LINKS: process.env.DELETE_LINKS || "false",
 // automatic delete links witho remove member 
-OWNER_NUMBER: process.env.OWNER_NUMBER || "254769876178",
+OWNER_NUMBER: process.env.OWNER_NUMBER || "263789745277",
 // add your bot owner number
-OWNER_NAME: process.env.OWNER_NAME || "𝙋𝙀𝘼𝘾𝙀𝙈𝘼𝙆𝙀𝙍💚",
+OWNER_NAME: process.env.OWNER_NAME || "KEITH-XMD💚",
 // add bot owner name
-DESCRIPTION: process.env.DESCRIPTION || "*© powered by Peace md*",
+DESCRIPTION: process.env.DESCRIPTION || "*© powered by KEITH*",
 // add bot owner name    
 ALIVE_IMG: process.env.ALIVE_IMG || "https://files.catbox.moe/n0dgjr.jpg",
 // add img for alive msg
